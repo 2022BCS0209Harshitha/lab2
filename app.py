4f21f106b2bf6d93f1acac92546f04982c3b799d
2022BCS0209Harshitha <harshitha22bcs209@iiitkottayam.ac.in>
2026-01-23 21:36:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,35 +6,20 @@ import numpy as np
 
 app = FastAPI(title="Wine Quality Predictor")
 
-# load trained model
 model = joblib.load("model.joblib")
 
-# ✅ this fixes Swagger schema
 class PredictRequest(BaseModel):
     features: List[float]
 
 @app.get("/")
 def root():
-    return {
-        "name": "Harshitha",
-        "roll_no": "2022BCS0209",
-        "status": "API running"
-    }
+    return {"name": "Harshitha", "roll_no": "2022BCS0209", "status": "API running"}
 
 @app.post("/predict")
-def predict(request: PredictRequest):
-    if len(request.features) != 11:
-        return {
-            "name": "Harshitha",
-            "roll_no": "2022BCS0209",
-            "error": "Exactly 11 features required"
-        }
+def predict(req: PredictRequest):
+    if len(req.features) != 11:
+        return {"error": "Exactly 11 values required"}
 
-    X = np.array(request.features, dtype=float).reshape(1, -1)
-    prediction = model.predict(X)[0]
-
-    return {
-        "name": "Harshitha",
-        "roll_no": "2022BCS0209",
-        "wine_quality": int(round(prediction))
-    }
+    X = np.array(req.features, dtype=float).reshape(1, -1)
+    pred = model.predict(X)[0]
+    return {"name": "Harshitha", "roll_no": "2022BCS0209", "wine_quality": int(round(pred))}
